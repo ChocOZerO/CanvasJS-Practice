95a0eec3fa5552a4f671d16d9504b15e0373d371
Taehyeon Jake Lee <nayalth@gmail.com>
2017-06-11 19:27:26 +0900

    Make a animation with a circle

--- Tutorial_ChrisCourses/canvas.js
+++ Tutorial_ChrisCourses/canvas.js
@@ -5,33 +5,60 @@ canvas.height = window.innerHeight;
 
 var c = canvas.getContext('2d');
 
-c.fillStyle = 'rgba(255, 0 , 0, 0.5)';
-c.fillRect(100, 100, 100, 100);
-c.fillStyle = 'rgba(0, 0, 255, 0.5)';
-c.fillRect(400, 100, 100, 100);
-c.fillStyle = 'rgba(0, 255, 0, 0.5)';
-c.fillRect(300, 300, 100, 100);
-console.log(canvas);
+// c.fillStyle = 'rgba(255, 0 , 0, 0.5)';
+// c.fillRect(100, 100, 100, 100);
+// c.fillStyle = 'rgba(0, 0, 255, 0.5)';
+// c.fillRect(400, 100, 100, 100);
+// c.fillStyle = 'rgba(0, 255, 0, 0.5)';
+// c.fillRect(300, 300, 100, 100);
+// console.log(canvas);
+//
+// // Line
+// c.beginPath();
+// c.moveTo(50, 300);
+// c.lineTo(300, 100);
+// c.lineTo(400, 300);
+// c.strokeStyle = "#fa34a3";
+// c.stroke();
+//
+// // Arc / Circle
+// c.beginPath();
+// c.arc(300, 300, 30, 0, Math.PI * 2, false);
+// c.strokeStyle = 'blue';
+// c.stroke();
+//
+// for (var i = 0; i < 100; i++) {
+//     var x = Math.random() * window.innerWidth;
+//     var y = Math.random() * window.innerHeight;
+//     c.beginPath();
+//     c.arc(x, y, 30, 0, Math.PI * 2, false);
+//     c.strokeStyle = 'blue';
+//     c.stroke();
+// }
 
-// Line
-c.beginPath();
-c.moveTo(50, 300);
-c.lineTo(300, 100);
-c.lineTo(400, 300);
-c.strokeStyle = "#fa34a3";
-c.stroke();
+var x = Math.random() * innerWidth;
+var y = Math.random() * innerHeight;
+var dx = (Math.random() - 0.5) * 8;
+var dy = (Math.random() - 0.5) * 8;
+var radius = 30;
+function animate() {
+    requestAnimationFrame(animate);
+    c.clearRect(0, 0, innerWidth, innerHeight);
 
-// Arc / Circle
-c.beginPath();
-c.arc(300, 300, 30, 0, Math.PI * 2, false);
-c.strokeStyle = 'blue';
-c.stroke();
-
-for (var i = 0; i < 100; i++) {
-    var x = Math.random() * window.innerWidth;
-    var y = Math.random() * window.innerHeight;
     c.beginPath();
-    c.arc(x, y, 30, 0, Math.PI * 2, false);
+    c.arc(x, y, radius, 0, Math.PI * 2, false);
     c.strokeStyle = 'blue';
     c.stroke();
+
+    if (x + radius > innerWidth || x - radius < 0) {
+        dx = -dx;
+    }
+
+    if (y + radius > innerHeight || y - radius < 0) {
+        dy = -dy;
+    }
+    x += dx;
+    y += dy;
 }
+
+animate();
